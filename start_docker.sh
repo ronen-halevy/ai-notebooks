f1de3c9d2f1545fb5ca3e7b805f26134235d22b8
ronen <ronen.halevy@chargeafter.com>
2021-07-03 19:47:40 +0300

    wip

---
+++ start_docker.sh
@@ -0,0 +1,1 @@
+docker run -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -e NB_USER=ronen -e NB_UID=ronen:ronen   -w /ronen -e CHOWN_HOME=yes  -v /home/ronen/mine/jupyther-workspace/ai-notebooks:/ronen -v /home/ronen/mine/jupyther-workspace/output:/ronen/output ronen567/jupyther-tensorflow
