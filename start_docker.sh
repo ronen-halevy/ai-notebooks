4f4fda1129c1a20b29b1266f902cfa8138ba3e87
ronen <ronen.halevy@chargeafter.com>
2021-07-11 00:48:34 +0300

    docker run

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,3 @@
-docker run -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -e NB_USER=ronen -e NB_UID=ronen:ronen   -w /ronen -e CHOWN_HOME=yes  -v /home/ronen/mine/jupyther-workspace/ai-notebooks:/ronen -v /home/ronen/mine/jupyther-workspace/output:/ronen/output ronen567/jupyther-tensorflow
+# docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -e NB_USER=ronen -e NB_UID=ronen:ronen   -w /ronen -e CHOWN_HOME=yes  -v /home/ronen/mine/jupyther-workspace/ai-notebooks:/ronen -v /home/ronen/mine/jupyther-workspace/output:/ronen/output  jupyter/tensorflow-notebook
+docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -e NB_USER=ronen -e NB_UID=ronen:ronen   -w /ronen -e CHOWN_HOME=yes -v "${PWD}":/work jupyter/tensorflow-notebook
+# docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "${PWD}":/work jupyter/tensorflow-notebook
