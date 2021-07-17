27e10d900ad528044d289870823f35fa9856a46b
ronen <ronen.halevy@chargeafter.com>
2021-07-18 00:56:36 +0300

    docker works with volume

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,4 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
-ARG BASE_CONTAINER=jupyter/scipy-notebook
-FROM $BASE_CONTAINER
+FROM jupyter/tensorflow-notebook:latest
 
-LABEL maintainer="Jupyter Project <jupyter@googlegroups.com>"
-
-# Install Tensorflow
-RUN mamba install --quiet --yes \
-    'tensorflow=2.4.1' && \
-    mamba clean --all -f -y && \
-    fix-permissions "${CONDA_DIR}" && \
-    fix-permissions "/home/${NB_USER}"
--- start_docker.sh
+++ start_docker.sh
@@ -1,3 +1,3 @@
-# docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -e NB_USER=ronen -e NB_UID=ronen:ronen   -w /ronen -e CHOWN_HOME=yes  -v /home/ronen/mine/jupyther-workspace/ai-notebooks:/ronen -v /home/ronen/mine/jupyther-workspace/output:/ronen/output  jupyter/tensorflow-notebook
-docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -e NB_USER=ronen -e NB_UID=ronen:ronen   -w /ronen -e CHOWN_HOME=yes -v "${PWD}":/work jupyter/tensorflow-notebook
-# docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "${PWD}":/work jupyter/tensorflow-notebook
+# not needed any more:
+#docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "${PWD}":/work -v "${PWD}"/notebooks:/notebooks jupyter/tensorflow-notebook
+docker-compose -f docker-compose-tensorflow.yml up
